3fa22497d56b062a5cf47ae2e65090b565f343cb
Sunhey Cho <sunheycho@mac.home>
2025-08-23 11:43:40 -0400

    Cleanup: remove static backup folders; deploy React build to static/static; update test images

---
+++ static/static/precache-manifest.653a7aee73b10eaaa2fae0610652f6fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63e79b8c5a7091459c975e9ced1d4412",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d435db4fd542a4ef6aa8",
+    "url": "/static/css/main.59c2a54e.chunk.css"
+  },
+  {
+    "revision": "a3aa56e81892b1b7fdee",
+    "url": "/static/js/2.abf91741.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/static/js/2.abf91741.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70d68b45d511e1e11f23",
+    "url": "/static/js/3.9013e23f.chunk.js"
+  },
+  {
+    "revision": "d435db4fd542a4ef6aa8",
+    "url": "/static/js/main.db88b5b1.chunk.js"
+  },
+  {
+    "revision": "d8c310b0ac7ffa6d8151",
+    "url": "/static/js/runtime-main.25710301.js"
+  }
+]);
